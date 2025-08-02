36f2b42bfb040599d6e871d84db0406fe9909b87
_mr_e7ro7_ <venkat.p.veeramallu@gmail.com>
2025-08-02 23:39:05 +0530

    merging

--- 21 dares.py
+++ 21 dares.py
@@ -1,8 +1,22 @@
 import time
+import random
 
-print("Computer is player 1")
+print("would you like see instructions?")
+inst = str(input("If yes then type 'Y' else type 'N'\n>>>"))
+if inst == 'Y':
+    print("you have to input 1, 2, or 3 numbers. the numbers must be consecutive. whoever reaches the number 21 first looses.")
 
-x = input("if you want to go first type 'F' else type 'S'")
+
+print("Computer is player 2")
+
+dare = []
+x = input("if you want to go first type 'F' else type 'S' \n>>>")
 if x == 'F':
+    u = int(input("How many numbers will you enter? \n>>>"))
+    for i in range(u):
+        a = int(input(">>>"))
+        dare.append(a)
+    print(dare)
+
 
 time.sleep(5)
