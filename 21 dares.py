2c6065374ff2e51a24c2cf03c840fc54b07a4d68
_mr_e7ro7_ <venkat.p.veeramallu@gmail.com>
2025-08-02 23:58:53 +0530

    update 21 dares.py

--- 21 dares.py
+++ 21 dares.py
@@ -6,17 +6,43 @@ inst = str(input("If yes then type 'Y' else type 'N'\n>>>"))
 if inst == 'Y':
     print("you have to input 1, 2, or 3 numbers. the numbers must be consecutive. whoever reaches the number 21 first looses.")
 
+def U_Turn():#your turn
+    u = int(input("How many numbers will you enter? \n>>>"))
+    for i in range(u):
+        if dare[-1] == 21:
+            print("You Lose. \nTry Again.")
+            break
+        else:
+            dare.append(dare[-1] + 1)
+    if 0 in dare:
+        dare.remove(0)
+    print(dare)
+
+def C_Turn():# computer turn
+    r = random.randint(1,4)
+    for i in range(r):
+        if dare[-1] == 21:
+            print("You Win. \nCongratulations!")
+            break
+        else:
+            dare.append(dare[-1] + 1)
+    if 0 in dare:
+        dare.remove(0)
+    print(dare)
+
+
+
+
+
 
 print("Computer is player 2")
 
 dare = []
 x = input("if you want to go first type 'F' else type 'S' \n>>>")
 if x == 'F':
-    u = int(input("How many numbers will you enter? \n>>>"))
-    for i in range(u):
-        a = int(input(">>>"))
-        dare.append(a)
-    print(dare)
+
+
+
 
 
 time.sleep(5)
